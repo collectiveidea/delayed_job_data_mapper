2e471c15e13510ec1013bbf08723d67859ac9ad3
Brian Ryckbost <bryckbost@gmail.com>
2011-11-09 00:02:55 -0500

    Add an encoder for psych parsing.

--- lib/delayed/serialization/data_mapper.rb
+++ lib/delayed/serialization/data_mapper.rb
@@ -1,12 +1,21 @@
 # encoding: utf-8
-DataMapper::Resource.class_eval do
-  yaml_as "tag:ruby.yaml.org,2002:DataMapper"
+if YAML.parser.class.name =~ /syck/i
+  DataMapper::Resource.class_eval do
+    yaml_as "tag:ruby.yaml.org,2002:DataMapper"
 
-  def self.yaml_new(klass, tag, val)
-    klass.get!(val['id'])
+    def self.yaml_new(klass, tag, val)
+      klass.get!(val['attributes']['id'])
+    end
+
+    def to_yaml_properties
+      ['@attributes']
+    end
   end
-
-  def to_yaml_properties
-    ['@id']
+else
+  DataMapper::Resource.class_eval do
+    def encode_with(coder)
+      coder["attributes"] = @attributes
+      coder.tag = ['!ruby/DataMapper', self.class.name].join(':')
+    end
   end
 end
