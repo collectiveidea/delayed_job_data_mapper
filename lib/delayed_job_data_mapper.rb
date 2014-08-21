f890f5f3d6923c5c28a1c1e8d880c56dfdd5ae97
Tony Pitale <tpitale@gmail.com>
2014-08-20 22:26:12 -0400

    Update dependency to dj ~> 4.0
    Refactor query conditionals into clearer "scopes"
    Adjust scopes slightly to better match logic in DJv4
    Changes in scopes seem to fix edge cases broken by me previously
    
    Trigger a record reload after we perform lock_exclusively

--- lib/delayed_job_data_mapper.rb
+++ lib/delayed_job_data_mapper.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require 'dm-core'
+require 'dm-active_model' # for persisted? alias for DJ 4.0.1
 require 'delayed_job'
 require 'delayed/serialization/data_mapper'
 require 'delayed/backend/data_mapper'
